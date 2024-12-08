226c8be7a2ab4a5785613fddb3e7659ce24dcae6
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-12-09 01:07:59 +0330

    fix: fix deployment github

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/m0k99/nexaportal-form-builder',
   plugins: [
     vue(),
     vueJsx(),
