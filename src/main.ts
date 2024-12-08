81ed182db49773131f0a132df90851e76b5a1b85
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-12-08 23:47:52 +0330

    feat: remove vue-json-viewer

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import JsonViewer from "vue3-json-viewer";
-// if you used v1.0.5 or latster ,you should add import "vue3-json-viewer/dist/index.css"
-import "vue3-json-viewer/dist/index.css";
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(JsonViewer);
 
 app.mount('#app')
