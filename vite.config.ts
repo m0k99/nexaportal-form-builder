61d34a5161fb8b78b30e6555972d23ae596176c0
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-12-09 01:06:40 +0330

    feat: fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/m0k99/nexaportal-form-builder',
   plugins: [
     vue(),
     vueJsx(),
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
