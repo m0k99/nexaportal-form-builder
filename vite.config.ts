95b94bc05c762b228b65af6c800bc57168aa15cf
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-12-08 23:59:15 +0330

    feat: add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/m0k99/nexaportal-form-builder',
   plugins: [
     vue(),
     vueJsx(),
